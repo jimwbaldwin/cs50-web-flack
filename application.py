0c23b06a74fd366aa2f6a70ab6084e991793cdc7
James Baldwin <jimwbaldwin@gmail.com>
2018-08-23 00:00:42 +1000

    Added socket.io. 1 channel, multiple user chat working.

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 import os
+import json
 
+from datetime import datetime
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
@@ -7,14 +9,30 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+maxChannelMessages = 100
+channels = {"general" : []}
+
 
 @app.route("/")
 def index():
-
     return render_template("index.html")
-
 
 @app.route("/chat")
 def chat():
     return render_template("chat.html")
 
+@socketio.on("submit message")
+def submit_message(data):
+    channel = data["channel"]
+    user = data["display_name"]
+    message = data["message"]
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    tempDict = {"user": user, "message": message, "time": time}
+    channels[channel].append(tempDict)
+    while len(channels[channel]) > maxChannelMessages:
+        channels[channel].pop(0)
+    emit("channel messages", json.dumps(channels[channel]), broadcast=True)
+
+def get_messages(data):
+    channel = data["channel"]
+    emit("channel messages", channels[channel], broadcast=False)
--- static/chat.js
+++ static/chat.js
@@ -3,18 +3,47 @@ if (!localStorage.getItem('display_name')) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Connect to websocket
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-  document.querySelector('#form-chat-input').onsubmit = () => {
-    const p  = document.createElement('p');
-    p.innerHTML = document.querySelector('#chat-input').value;
+  // When connected, configure buttons
+  socket.on('connect', () => {
 
-    // Add paragraph to display
-    document.querySelector('#chat-display').appendChild(p);
+    document.querySelector('#form-chat-input').onsubmit = () => {
+      socket.emit('submit message', {"channel": "general"
+                , "display_name": localStorage.getItem('display_name')
+                , "message": document.querySelector('#chat-input').value
+              });
 
-    // Clear input field
-    document.querySelector('#chat-input').value = '';
+      // Clear input field
+      document.querySelector('#chat-input').value = '';
+      
+      // Stop form from submitting
+      return false;
+    };
+  });
+  // When a new vote is announced, add to the unordered list
+  socket.on('channel messages', data => {
+    const ul  = document.createElement('ul');
+    myNode = document.querySelector('#chat-display')
+    while (myNode.firstChild) {
+      myNode.removeChild(myNode.firstChild);
+    }
+    document.querySelector('#chat-display').append(ul);
+
+    var jsonData = JSON.parse(data);
+    for (var i = 0; i < jsonData.length; i++) {
+        var message = jsonData[i];
+        var li = document.createElement('li')
+        li.innerHTML = message.message;
+        ul.appendChild(li)
+        console.log(message.message);
+    };
     
-    // Stop form from submitting
-    return false;
-  };
+    // Add paragraph to display
+    
+    //document.querySelector('#yes').innerHTML = data.yes;
+    //document.querySelector('#no').innerHTML = data.no;
+    //document.querySelector('#maybe').innerHTML = data.maybe;
+  });
 });
