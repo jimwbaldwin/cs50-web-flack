245d858adc4ef32b94b18b665cee37fa5396af57
James Baldwin <jimwbaldwin@gmail.com>
2018-08-22 20:34:13 +1000

    Added client side, single user chat.

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -10,4 +10,11 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+
+    return render_template("index.html")
+
+
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
+
